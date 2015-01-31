733dcda86e6376070fb9f4e169627a187167ae4e
Tom Reece <tomreece@gmail.com>
2015-01-31 13:23:41 -0500

    Needs cleanup: Add messaging, increase range of nearby players to 50~ miles, only ask for location on the Nearby page.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import datetime
+import pytz
 from flask import Flask, abort, request, jsonify, redirect, render_template, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import or_
 from flask.ext.login import LoginManager, login_required, login_user, current_user, logout_user
 from flask.ext.bcrypt import Bcrypt
 from marshmallow import Serializer
@@ -52,6 +54,50 @@ class User(db.Model):
         return unicode(self.id)
     # /Flask-Login functions
 
+    def pretty_last_active(self):
+        """
+        Get a datetime object or a int() Epoch timestamp and return a
+        pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+        'just now', etc
+        """
+        from datetime import datetime
+        time = self.last_active
+        now = datetime.now(pytz.utc)
+        if type(time) is int:
+            diff = now - datetime.fromtimestamp(time)
+        elif isinstance(time,datetime):
+            diff = now - time
+        elif not time:
+            diff = now - now
+        second_diff = diff.seconds
+        day_diff = diff.days
+
+        if day_diff < 0:
+            return ''
+
+        if day_diff == 0:
+            if second_diff < 10:
+                return "just now"
+            if second_diff < 60:
+                return str(second_diff) + " seconds ago"
+            if second_diff < 120:
+                return "a minute ago"
+            if second_diff < 3600:
+                return str(second_diff / 60) + " minutes ago"
+            if second_diff < 7200:
+                return "an hour ago"
+            if second_diff < 86400:
+                return str(second_diff / 3600) + " hours ago"
+        if day_diff == 1:
+            return "Yesterday"
+        if day_diff < 7:
+            return str(day_diff) + " days ago"
+        if day_diff < 31:
+            return str(day_diff / 7) + " weeks ago"
+        if day_diff < 365:
+            return str(day_diff / 30) + " months ago"
+        return str(day_diff / 365) + " years ago"
+
     class Serializer(Serializer):
         class Meta:
             fields = ("id", "name", "lat", "lon", "last_active")
@@ -71,6 +117,18 @@ class Card(db.Model):
     class Serializer(Serializer):
         class Meta:
             fields = ("id", "name", "user_id")
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    from_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    from_user = db.relationship('User', primaryjoin=(from_user_id == User.id), backref=db.backref('from', lazy='dynamic'))
+    to_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    to_user = db.relationship('User', primaryjoin=(to_user_id == User.id), backref=db.backref('to', lazy='dynamic'))
+    message = db.Column(db.String)
+    time = db.Column(db.DateTime(timezone=True), index=True)
+    is_read = db.Column(db.Boolean, default=False, server_default='FALSE')
 
 #
 # ROUTES
@@ -116,23 +174,29 @@ def home():
     cards = current_user.cards.order_by(db.func.lower(Card.name)).all()
     return render_template('home.html', cards=cards)
 
+@app.route('/location')
+@login_required
+def location():
+    return render_template('location.html')
+
 @app.route('/nearby')
 @login_required
 def nearby():
-    DISTANCE_THRESHOLD = 0.003
-    TIME_THRESHOLD = 15 # minutes
-    if current_user.lat and current_user.lon:
+    DISTANCE_THRESHOLD = 0.6 # about 50 miles
+    TIME_THRESHOLD = 5 # minutes
+    if current_user.lat and current_user.lon and \
+            current_user.last_active > datetime.datetime.now(pytz.utc) - datetime.timedelta(minutes=TIME_THRESHOLD):
         users = (User.query
-            .filter(User.last_active > db.func.now() - datetime.timedelta(minutes=TIME_THRESHOLD))
+            #.filter(User.last_active > db.func.now() - datetime.timedelta(minutes=TIME_THRESHOLD))
             .filter(User.id != current_user.id)
             .filter(User.lat > current_user.lat - DISTANCE_THRESHOLD)
             .filter(User.lat < current_user.lat + DISTANCE_THRESHOLD)
             .filter(User.lon > current_user.lon - DISTANCE_THRESHOLD)
             .filter(User.lon < current_user.lon + DISTANCE_THRESHOLD)
-            .order_by(db.func.lower(User.name))
+            .order_by(User.last_active.desc())
             .all())
     else:
-        users = []
+        return redirect(url_for('location'))
     return render_template('nearby.html', users=users)
 
 @app.route('/card/<int:card_id>/remove')
@@ -178,7 +242,32 @@ def user_location():
 @app.route('/messages')
 @login_required
 def messages():
-    return render_template('messages.html')
+    messages = (Message.query
+        .filter(or_(Message.to_user_id == current_user.id, Message.from_user_id == current_user.id))
+        .order_by(Message.time.desc())
+        .limit(50)
+        .all())
+    return render_template('messages.html', messages=messages)
+
+@app.route('/messages/send/<int:to_user_id>')
+@login_required
+def messages_send(to_user_id):
+    to_user = User.query.get(to_user_id)
+    return render_template('send_message.html', to_user=to_user)
+
+@app.route('/messages/send', methods=['POST'])
+@login_required
+def messages_send_post():
+    to_user_id = request.form['to_user_id']
+    message_body = request.form['message']
+    message = Message()
+    message.from_user_id = current_user.id
+    message.to_user_id = to_user_id
+    message.message = message_body
+    message.time = db.func.now()
+    db.session.add(message)
+    db.session.commit()
+    return redirect(url_for('messages'))
 
 #
 # MAIN
